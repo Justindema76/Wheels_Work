a075dad718a2c8f8cae99a7ec3540e5e60f30d87
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-08-18 22:08:10 -0400

    Fix unified designer paths for local Live Server

--- Plate-Cover-Designer/unified/unified-config.js
+++ Plate-Cover-Designer/unified/unified-config.js
@@ -3,28 +3,28 @@ window.WHEELS_DESIGNERS = [
     id: 'frame',
     name: 'Plate Frame Designer',
     description: 'Licence plate frame designer',
-    source: '../plate-frame-designer.html',
+    source: 'Plate-Cover-Designer/plate-frame-designer.html',
     production: ['screen-print']
   },
   {
     id: 'plate-sign',
     name: 'Plate Sign Designer',
     description: 'Renamed from Plate Cover Designer',
-    source: '../plate-cover-designer.html',
+    source: 'Plate-Cover-Designer/plate-cover-designer.html',
     production: ['digital-print']
   },
   {
     id: 'lexan',
     name: 'Lexan Plate Cover Designer',
     description: 'Clear Lexan plate cover designer',
-    source: '../lexan-plate-designer.html',
+    source: 'Plate-Cover-Designer/lexan-plate-designer.html',
     production: ['screen-print', 'digital-print']
   },
   {
     id: 'motorcycle',
     name: 'Motorcycle Plate Cover Designer',
     description: 'Motorcycle plate cover designer',
-    source: '../motorcycle-plate-cover-designer.html',
+    source: 'Plate-Cover-Designer/motorcycle-plate-cover-designer.html',
     production: ['digital-print']
   }
 ];
