2f4b53546e3cfd3effd98a5f7c7b93a82af298a7
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-09-23 19:13:56 -0400

    Bust cache for snap guide cleanup

--- Wheels-Design-Studio/js/wheels-designers.js
+++ Wheels-Design-Studio/js/wheels-designers.js
@@ -12,7 +12,7 @@
   }
 
   try{
-    const response=await fetch('js/wheels-designers-core.js?v=tangent-zones-1',{cache:'no-store'});
+    const response=await fetch('js/wheels-designers-core.js?v=guide-reset-20260923-1912',{cache:'no-store'});
     if(!response.ok) throw new Error('Could not load designer core: '+response.status);
 
     let source=await response.text();
