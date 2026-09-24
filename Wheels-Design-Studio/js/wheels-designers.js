47822e57eb86137bb42de569f0c9c90257a2e29a
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-09-23 20:04:38 -0400

    Load fixed three-zone frame geometry

--- Wheels-Design-Studio/js/wheels-designers.js
+++ Wheels-Design-Studio/js/wheels-designers.js
@@ -12,7 +12,7 @@
   }
 
   try{
-    const response=await fetch('js/wheels-designers-core.js?v=screen-one-colour-20260923-1922',{cache:'no-store'});
+    const response=await fetch('js/wheels-designers-core.js?v=top-3-mechanical-zones-20260923-2004',{cache:'no-store'});
     if(!response.ok) throw new Error('Could not load designer core: '+response.status);
 
     let source=await response.text();
