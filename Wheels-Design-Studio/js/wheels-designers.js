e3981a070e0e2359e9fc8684538bcd5b6fc71885
Justin DeMatteis <160768253+Justindema76@users.noreply.github.com>
2026-09-24 22:14:18 -0400

    Reload reverted Lexan core

--- Wheels-Design-Studio/js/wheels-designers.js
+++ Wheels-Design-Studio/js/wheels-designers.js
@@ -12,7 +12,7 @@
   }
 
   try{
-    const response=await fetch('js/wheels-designers-core.js?v=lexan-product-config-20260924-2215',{cache:'no-store'});
+    const response=await fetch('js/wheels-designers-core.js?v=lexan-revert-20260924-2218',{cache:'no-store'});
     if(!response.ok) throw new Error('Could not load designer core: '+response.status);
 
     let source=await response.text();
